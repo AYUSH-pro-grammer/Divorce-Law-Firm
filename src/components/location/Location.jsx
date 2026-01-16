15d9af0f0691bd2d2cd6bafaf5ef148bf6b6b8bb
Ayush Kumar <167435819+AYUSH-pro-grammer@users.noreply.github.com>
2026-01-17 01:11:50 +0530

    Update Location.jsx

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -27,14 +27,15 @@ const Location = () => {
     ></iframe> */}
 
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.049955892218!2d77.48110517580048!3d28.50814468971776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ceb35e4b01f37%3A0xa5dff112a4255735!2sAyush%20Law%20firm!5e0!3m2!1sen!2sin!4v1768592322203!5m2!1sen!2sin"
+<iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.049955892218!2d77.48110517580048!3d28.50814468971776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ceb35e4b01f37%3A0xa5dff112a4255735!2sAyush%20Law%20firm!5e0!3m2!1sen!2sin!4v1768592322203!5m2!1sen!2sin"
   width="100%"
-  height="450" 
-  style="border:0;"
-  allowfullscreen="" 
-  loading="lazy" 
-  referrerpolicy="no-referrer-when-downgrade"></iframe>
-              
+  height="450"
+  style={{ border: 0 }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+/>
 
             
             </div>
